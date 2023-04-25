eb1123261e98d8d20bac976505c4afc7bdaa858e
Allankyaw <kyaw87.2000@gmail.com>
2023-04-26 01:05:08 +0800

    Got the buttons to work
    Need to move the button as a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,49 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  const [data, setData] = useState([]);
-  const getData = async () => {
+  const [dogImage, setDogImage] = useState(null);
+  const [catImage, setCatImage] = useState(null);
+
+  const handleDogClick = async () => {
     const res = await fetch(
-      "https://api.thedogapi.com/v1/images/search?limit=3"
+      "https://api.thedogapi.com/v1/images/search?limit=1"
     );
     if (res.status === 200) {
       const data = await res.json();
-      setData(data);
+      setDogImage(data[0].url);
     } else {
       alert(`Something went wrong: ${res.status} code returned`);
     }
   };
-  useEffect(() => {
-    getData();
-  }, []);
+
+  const handleCatClick = async () => {
+    const res = await fetch(
+      "https://api.thecatapi.com/v1/images/search?limit=1"
+    );
+    if (res.status === 200) {
+      const data = await res.json();
+      setCatImage(data[0].url);
+    } else {
+      alert(`Something went wrong: ${res.status} code returned`);
+    }
+  };
+
   return (
     <>
-      {data.map((fact) => (
-        <div key={fact.url}>{fact.url}</div>
-      ))}
+      <button onClick={handleDogClick}>Get a dog</button>
+      {dogImage && (
+        <div>
+          <img src={dogImage} alt="Dog" />
+        </div>
+      )}
+
+      <button onClick={handleCatClick}>Get a cat</button>
+      {catImage && (
+        <div>
+          <img src={catImage} alt="Cat" />
+        </div>
+      )}
     </>
   );
 }
